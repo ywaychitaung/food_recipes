1ed4e0dc580563b67a80f6850746bd5ab1dea8e8
ywaychitaung <ywaychitaung@gmail.com>
2023-06-12 11:44:23 +0630

    Add Javascript

--- recipes/static/js/main.js
+++ recipes/static/js/main.js
@@ -1,1 +1,13 @@
-document.getElementById('logo').innerHTML = "Recipe"
+document.getElementById('logo').innerHTML = "Recipes"
+
+const footer = document.getElementById('footer')
+footer.style.color = "white";
+footer.style.width = "80%";
+footer.style.borderTopLeftRadius = "1rem";
+footer.style.borderTopRightRadius = "1rem";
+footer.style.textAlign = "center";
+footer.style.backgroundColor = "rgba(25, 135, 84, 1)";
+footer.style.marginLeft = "auto";
+footer.style.marginRight = "auto";
+footer.style.paddingTop = "1rem";
+footer.style.paddingBottom = "1rem";
